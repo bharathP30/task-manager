ab249aa92979b5f5ff1ac587b9ff26041431374b
bharathP30 <bharath145p@gmail.com>
2025-12-21 14:03:47 +0530

    add search task section and some minor styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
     const [categoryfilter, setCategoryfilter] = useState("");
     const [priorityfilter, setPriorityfilter] = useState("");
     const [searchterm, setSearchterm] = useState("");
-    const [status, setStatus] = useState("false");
+    const [statusFilter, setStatusfilter] = useState("All");
 
     const addTask = () => {
                 if (task.trim().length === 0) {
@@ -25,46 +25,80 @@ export default function App() {
                 dueDate: date,     
                 completed: false,       
                 }
+
             setList([...list, data]);
             setTask("");
         
     };
 
-    const isDone = "text-sm text-gray-200 line-through opacity-50 transition duration-300";
-    const notDone = "text-sm text-gray-100 transition duration-300";
-    const btn = "text-md text-center w-fit px-4 py-1 rounded-full transition-all duration-300 focus:scale-105 ";
-    const active = "bg-gray-500 ";
-    const inactive = "bg-gray-600 text-gray-100 ";
+    const isDone = "text-sm text-gray-200 md:text-xl line-through opacity-50 transition duration-300";
+    const notDone = "text-sm text-gray-100 md:text-xl transition duration-300";
+    const btn = "text-md text-center w-fit px-4 py-1 rounded-full md:text-lg transition-all duration-300 focus:scale-105 ";
+    const active = "bg-gray-500 shadow-sm";
+    const inactive = "bg-gray-700 text-gray-100 ";
 
     const filteredList = list.filter(entry => {
+            const term = entry.content.trim().includes(searchterm.trim());
+            const taskStatus = statusFilter === "All" || statusFilter === entry.completed
             const cat = categoryfilter === "All" || categoryfilter === entry.type;
             const prio = priorityfilter === "All" || priorityfilter === entry.order;
 
-            return cat && prio;
+            return term && taskStatus && cat && prio;
     });
 
 return (
     <div className="min-h-screen w-full p-4 bg-gradient-to-b from-gray-900 via-gray-600 to-gray-900">
     
         <header className="">
-            <h1 className="text-3xl text-gray-400 font-sans font-bold text-center my-6 ">Task Manager</h1>
+            <h1 className="text-3xl text-gray-400 font-sans font-bold text-center my-6
+                          md:text-6xl md:my-12
+                            ">Task Manager</h1>
         </header>
 
-        <div className='w-full p-4 text-center mb-4 md:mx-auto md:max-w-4xl md:p-8 md:text-xl text-white rounded-lg font-sans bg-gradient-to-br from-gray-900 to-gray-700'>
-            <label className="inline mx-auto" htmlFor="taskInput">
+        <div className="w-full space-x-1 bg-gray-900 p-2 my-4 rounded-lg flex
+                        md:max-w-xl md:mx-auto md:space-x-2 
+                        ">
+            
+             <select className="w-fit text-center rounded-md py-1 outline-none appearance-none
+                                 bg-gray-800 text-gray-300"
+                    name="statusFilter" id="statusFilter"
+                    value={statusFilter}
+                    onChange={(e)=>setStatusfilter(e.target.value)}
+                    >
+                    <option value="All">All</option>
+                    <option value="true">Done</option>
+                    <option value="false">Pending</option>
+             </select>
+            <input type="search"
+             className="rounded-lg w-full bg-gray-800 text-gray-100 h-10 outline-none p-2
+                       focus:ring-1 focus:ring-blue-500"
+             value={searchterm}
+             placeholder="Search tasks..."
+             onChange={(e)=>setSearchterm(e.target.value)}
+             />
+
+        </div>
+
+        <div className='w-full p-4 text-center mb-4 text-white rounded-lg font-sans bg-gradient-to-b from-gray-900 to-gray-700
+                        md:mx-auto md:max-w-3xl md:p-8 md:text-lg md:my-12'>
+        
             <input required
-                   className='w-full mx-auto rounded-lg p-2 m-2 outline-none font-sans text-black focus:shadow-sm' 
+                   className='w-full mx-auto rounded-lg p-2 m-2 outline-none
+                             text-gray-100 bg-gray-700 font-sans 
+                             focus:ring-1 ring-green-400' 
                    onChange={(e) => setTask(e.target.value)} 
                    type="text" 
                    id="taskInput" 
                    value={task} 
                    placeholder='Enter Task...'/>
-            </label>
+         
 
-            <div className='flex justify-between items-end'>
+            <div className='flex justify-between items-end md:justify-center md:gap-4'>
 
                     <select  
-                    className='rounded-md border-none text-center w-fit py-1 md:max-w-40 md:mx-4 bg-gray-600 text-white font-sans' 
+                    className='rounded-md text-center w-fit py-1 
+                                md:w-1/4 bg-gray-600 text-white 
+                                font-sans outline-none' 
                     onChange={(e) => setCategory(e.target.value)} 
                     name="categoryType" 
                     id="categoryType"
@@ -79,7 +113,9 @@ return (
                     </select>
 
                     <select 
-                    className='rounded-md border-none text-center max-w-16 py-1 md:max-w-40 md:mx-4 bg-gray-600 text-white font-sans' 
+                    className='rounded-md outline-none text-center w-1/5 py-1
+                         bg-gray-600 text-white font-sans
+                      ' 
                     onChange={(e) => setPriority(e.target.value)} 
                     name="priorityType" 
                     id="priorityType"
@@ -93,7 +129,8 @@ return (
 
                     <label htmlFor="deadDate">
                     <input required
-                    className='rounded-lg py-1 text-center border-none font-sans w-fit md:max-w-40 text-black' 
+                    className='rounded-lg py-1 text-center outline-none font-sans w-fit
+                    bg-gray-600 text-white md:max-w-40' 
                     onChange={(e) => setDate(e.target.value)} 
                     type="date" 
                     id="deadDate" 
@@ -104,16 +141,18 @@ return (
             <button 
                 onClick={addTask} 
                 disabled={task.trim().length === 0}
-                className={`text-white text-md w-1/3 mt-4 text-center p-2 rounded-md transition-all duration-200 
+                className={`text-white text-md w-1/3 mt-4 text-center p-2 rounded-md 
+                    transition-all duration-200 md:mt-8
                          ${task.trim().length === 0 
-                            ? 'bg-gray-500 cursor-not-allowed' 
+                            ? 'bg-gray-500 cursor-not-allowed text-gray-700' 
                             : 'bg-green-500 active:scale-105 active:bg-green-600'}`}
                         >
                         Add Task
             </button>
         </div>
         
-        <div className="space-x-1 space-y-1 flex justify-center items-center flex-wrap mb-4">
+       <div className="md:mb-8">
+         <div className="space-x-1 space-y-1 flex justify-center items-center flex-wrap mb-2  md:space-x-2 md:space-y-2">
             <button  
             onClick={()=>(setCategoryfilter("All"))}
             className={`${btn}${categoryfilter === "All"? active : inactive}`}>All</button>
@@ -134,7 +173,7 @@ return (
             className={`${btn}${categoryfilter === "Others"? active : inactive}`}>Others</button>
         </div>
 
-        <div className="mx-auto space-x-1 space-y-1 flex justify-center items-center flex-wrap mb-4">
+        <div className="mx-auto space-x-1 space-y-1 flex justify-center items-center flex-wrap mb-2  md:space-x-2 md:space-y-2">
             <button
             onClick={()=>(setPriorityfilter("All"))}
             className={`${btn}${priorityfilter === "All"? active : inactive}`}>All</button>
@@ -149,19 +188,24 @@ return (
             className={`${btn}${priorityfilter === "Low"? active : inactive}`}>Low</button>
         
         </div>
+       </div>
 
-        <div className="w-full p-4 text-center mb-4 md:mx-auto md:max-w-4xl md:p-8 md:text-xl text-white rounded-lg font-sans bg-gradient-to-br from-gray-900 to-gray-700">
-                <h2 className="text-center font-sans font-semibold text-xl text-gray-100">Tasks</h2>
+        <div className="w-full p-4 text-center mb-4 
+        md:mx-auto md:max-w-3xl md:p-8
+        text-white rounded-lg font-sans bg-gradient-to-b from-gray-900 to-gray-800">
+                <h2 className="text-center font-sans font-semibold text-xl text-gray-100 mb-4 md:mb-8 md:text-2xl">Tasks</h2>
             {   list.length === 0 ? (
-                <p className="text-gray-500 mb-6">No tasks yet</p>
+                <p className="text-gray-500 m-8">No tasks yet</p>
             ):(
                 <ul className="space-y-2"> 
                          {
                             filteredList.map(entry =>(
                             <li key={entry.id}
-                                className="flex flex-col gap-2 my-2 p-4 rounded-md space-y-1 bg-gradient-to-br from-black to-gray-800 transition-all duration-500"
+                                className="flex flex-col gap-2 my-2 p-4 rounded-md space-y-1
+                                           md:p-8
+                                bg-gradient-to-br from-black to-gray-800 transition-all duration-500"
                             >
-                               <div className="flex justify-between items-center">
+                               <div className="flex justify-between items-center gap-2">
                                     <p
                                         onClick={() => {
                                                 setList(list.map(item => 
@@ -185,7 +229,7 @@ return (
                                     >{entry.order} | {entry.type}
                                     </p>
 
-                                    <p className="font-mono ">Due: {entry.dueDate}</p>
+                                    <p className="font-mono text-sm">Due: {entry.dueDate}</p>
                                </div>
                                 
                             </li>
